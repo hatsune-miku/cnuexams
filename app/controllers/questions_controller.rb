3eee3bae6900a98a623c9d345254e3280b649c37
hatsunemiku <eggtart@mikutart.com>
2020-04-22 11:23:29 +0800

    20200422

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,12 @@ class QuestionsController < ApplicationController
     end
 
     def index
-        crud Question, '题目', '道'
+        case params[:intent]
+        when 'list', 'retrieve'
+            finish_with Question.where(params[:where]).limit(params[:limit] || 1000).offset(params[:offset] || 1000).order(:label)
+        
+        else
+            crud Question, '题目', '道'
+        end
     end
 end
